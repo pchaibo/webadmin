e4397145882ab517259b9f4f9edd5f8c25b1ceff
pchaibo <pchaibo@163.com>
2026-09-21 09:50:21 +0800

    totalincome

--- Backend/binan/task.go
+++ Backend/binan/task.go
@@ -337,9 +337,10 @@ func Checkheyun(user *model.User, heyue *model.Heyue, resdata []PositionRisk) (r
 			//网格补仓 2:保证金百分比计算
 			if heyue.Is_num < heyue.Num && heyue.Newprice > 0 {
 				//Marginpercentage := math.Abs(v.UnRealizedProfit) / margin
-				Marginpercentage := math.Abs((math.Abs(newmargin) - margin*20)) / margin //计算在原保证金的比例
+				totalincome := math.Abs(newmargin) - margin*20
+				Marginpercentage := math.Abs(totalincome) / margin //计算在原保证金的比例
 				Logs.Println("v  : ", v)
-				Logs.Println("margin", newmargin, margin)
+				Logs.Println("totalincome", newmargin, margin, totalincome)
 				Logs.Println("Checkadd : ", v.UnRealizedProfit, Marginpercentage, Rangepercent)
 
 				if v.UnRealizedProfit < 0 && Marginpercentage > Rangepercent {
