b56c31e21f3dbdddfd10ea367355351b9e16c1f6
pchaibo <pchaibo@163.com>
2026-09-23 08:44:03 +0800

    Ckeckrisk upd

--- Backend/binan/task.go
+++ Backend/binan/task.go
@@ -270,7 +270,7 @@ func Checkadd(user *model.User, heyue *model.Heyue, num int32) {
 	}
 
 	Logs.Println("Checkadd  : ", user.Username, heyue.Symbol, heyue.Oneprice, heyue.Repeatprice)
-	Addpositon(user, heyue)
+	//Addpositon(user, heyue)
 
 }
 
@@ -362,7 +362,7 @@ func Checkheyun(user *model.User, heyue *model.Heyue, resdata []PositionRisk) (r
 					}
 
 					Logs.Println("保证金百分比计算SHORT:", v.positionSide, Rangepercent, v.UnRealizedProfit, v.MarkPrice)
-					//Checkadd(user, heyue, 2)
+					Checkadd(user, heyue, 2)
 				}
 			}
 
